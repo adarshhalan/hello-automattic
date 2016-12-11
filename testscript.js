ec081a756d2838a6d2165d081a8989d9bee79c08
Spen Taylor <spen_@hotmail.co.uk>
2016-12-11 19:31:23 +1100

    Auto builder

--- testscript.js
+++ testscript.js
@@ -11,13 +11,13 @@ fs.rename('./.gitignore', './.gitignore.original', function(err) {
 		    } else {
 
 		    	// shelljs.exec('git add dist && git push origin \'git subtree split --prefix output gh-pages\':gh-pages --force');
-		    	shelljs.exec('git add dist && git commit -am \'Auto builder\' && git subtree push --prefix dist origin testy && git rm -r dist');
+		    	shelljs.exec('git add dist && git commit -am \'Auto builder\' && git subtree push --prefix dist origin testy && git rm -r --cached dist');
 
-		    	fs.rename('./.gitignore.publish', './.gitignore', function(err) {
+		    	fs.rename('./.gitignore', './.gitignore.publish', function(err) {
 				    if ( err ) {
 				    	console.log('ERROR: ' + err)
 				    } else {
-				    	fs.rename('./.gitignore', './.gitignore.original', function(err) {
+				    	fs.rename('./.gitignore.original', './.gitignore', function(err) {
 						    if ( err ) {
 						    	console.log('ERROR: ' + err)
 						    }
